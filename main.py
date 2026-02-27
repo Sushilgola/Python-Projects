ad5965106ec00b3bf41086cc9d8c0fb3d980a4a1
Sushil Kumar <112944863+Sushilgola@users.noreply.github.com>
2026-02-27 15:31:44 +0530

    Calculator

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
-# FIND THE RANDOM NUMBER & SUGGEST THAT I # # # WILL GREATER OR LESSER.
+num1 = float(input("Enter Number 1: "))
+opr = input("Enter operation symbols: ")
+num2 = float(input("Enter Number 2: "))
 
-import random
-
-chance = 0
-rand_num = random.randint(1, 100)
-
-print("You have 10 chance to find the Number! ")
-while chance != 10:
-    chance += 1
-    guess_num = int(input("Guess the number: "))
-
-    if rand_num == guess_num:
-        print("Congratulation You Got Number! ", guess_num)
-        break
-    elif rand_num > guess_num:
-        print("Try Bigger Number! ")
-    elif rand_num < guess_num:
-        print("Try Smaller Number! ")
-
-if chance == 10:
-    print("\nYou Lose the Game!")
-
+try:
+    if opr == '+':
+        print("Sum : ",num1+num2)
+    elif opr == '-':
+        print("Subtraction: ", num1-num2)
+    elif opr == '*':
+        print("Multiplication ", num1*num2)
+    elif opr == '/':
+        print("Division: ", num1/num2)
+except ZeroDivisionError:
+    print("We can't divide any number with 0. ")
+except ValueError:
+    print("Please Enter the Numeric Values.")
