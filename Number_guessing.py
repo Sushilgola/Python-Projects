671f2bf8dc63ed8401d067900bb8e495bfeb4328
Sushil Kumar <112944863+Sushilgola@users.noreply.github.com>
2026-03-16 22:51:08 +0530

    Add files via upload

---
+++ Number_guessing.py
@@ -0,0 +1,28 @@
+import random
+
+again = "y"
+while again == "y":
+    chance = 0
+    rand_num = random.randint(1, 100)
+
+    print("You have 10 chance to find the Number! ")
+    while chance != 10:
+        chance += 1
+        guess_num = int(input("Guess the number: "))
+
+        if rand_num == guess_num:
+            print("Congratulation You Got Number! ", guess_num)
+            break
+        elif rand_num > guess_num:
+            print("Try Bigger Number! ")
+        elif rand_num < guess_num:
+            print("Try Smaller Number! ")
+
+    if chance == 10:
+        print("\nYou Lose the Game!")
+
+    again = input("Do you want to Run Again (y/n)")
+    
+print("You Exit From Program.")
+
+
